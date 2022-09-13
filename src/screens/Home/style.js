141f91f7df66b4460d143066c4b1a87f4e778eee
DevOski <designsfunnel@gmail.com>
2022-09-13 15:47:25 +0500

    Home screen start

---
+++ src/screens/Home/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { height, width } from 'react-native-dimension';
+import { colors, fontFamily, fontSize, sizes } from '../../services';
+
+export const styles = StyleSheet.create({
+    color:{
+        color:colors.white
+    }
+})
