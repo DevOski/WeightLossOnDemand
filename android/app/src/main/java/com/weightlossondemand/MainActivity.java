2ee13bf7d04e5228555c751c7263bba7c25be07c
DevOski <designsfunnel@gmail.com>
2022-09-16 23:26:00 +0500

    splashscreen

--- android/app/src/main/java/com/weightlossondemand/MainActivity.java
+++ android/app/src/main/java/com/weightlossondemand/MainActivity.java
@@ -3,13 +3,20 @@ package com.weightlossondemand;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+ @Override                                             // add this
+  protected void onCreate(Bundle savedInstanceState) {  // add this
+    SplashScreen.show(this);                            // add this
+    super.onCreate(savedInstanceState);                 // add this
+  }  
+
   @Override
   protected String getMainComponentName() {
     return "WeightLossOnDemand";
