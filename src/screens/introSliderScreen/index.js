215f54507db521330344b130bf87c8873a82c359
DevOski <designsfunnel@gmail.com>
2022-09-21 16:29:22 +0500

    updated

--- src/screens/introSliderScreen/index.js
+++ src/screens/introSliderScreen/index.js
@@ -6,64 +6,47 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-const deviceHeight = Dimensions.get('window').height;
-const deviceWidth = Dimensions.get('window').width;
-import {
-  Component,
-  Componentsecond,
-  Componentthrd,
-  ComponentFour,
-} from '../../component/IntrComponent';
-import {colors, fontSize, sizes} from '../../services';
-// FontAwesome
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
-const Intro = ({navigation}) => {
-  const slides = [
-    {id: 1, name: <Component />},
-    {id: 2, name: <Componentsecond />},
-    {id: 3, name: <Componentthrd />},
-    {id: 4, name: <ComponentFour />},
-  ];
-  const [imgActive, setImgActive] = useState(0);
-
-  const onchange = nativeEvent => {
-    if (nativeEvent) {
-      const slide = Math.ceil(
-        nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
-      );
-      if (slide != imgActive) {
-        setImgActive(slide);
-      }
-    }
-  };
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <ScrollView
-          onScroll={({nativeEvent}) => onchange(nativeEvent)}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          style={styles.srollwidth}
-          disableIntervalMomentum={true}>
-          {slides.map((item, index) => {
-            return <View>{item.name}</View>;
-          })}
-        </ScrollView>
-        {/* {slides?.map((item, index) => {
+ import React, {useState} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   Dimensions,
+   TouchableOpacity,
+ } from 'react-native';
+ const deviceHeight = Dimensions.get('window').height;
+ const deviceWidth = Dimensions.get('window').width;
+ import {
+   Component,
+   Componentsecond,
+   Componentthrd,
+   ComponentFour,
+ } from '../../component/IntrComponent/index';
+ const App = () => {
+   const slides = [
+     {id: 1, name: <Component />},
+     {id: 2, name: <Componentsecond />},
+     {id: 3, name: <Componentthrd />},
+     {id: 4, name: <ComponentFour />},
+   ];
+   const [hideButton, setHideButton] = useState(false);
+   let test;
+   return (
+     <SafeAreaView>
+       <ScrollView
+         horizontal
+         showsHorizontalScrollIndicator={false}
+         style={styles.srollwidth}
+         disableIntervalMomentum={true}>
+         {slides.map((item, index) => {
+           return <View>{item.name}</View>;
+         })}
+       </ScrollView>
+       {slides?.map((item, index) => {
          console.log(item.id);
          return (
            <>
@@ -86,120 +69,56 @@ const Intro = ({navigation}) => {
              ) : null}
            </>
          );
-      //  })} */}
-        {/* {
-        imgActive !==3 && (
-          
-        )
-      } */}
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-            padding: 20,
-          }}>
-          {imgActive !== 3 && (
-            <View style={styles.footer}>
-              <TouchableOpacity onPress={()=>navigation.navigate("signinscreen")}>
-                <Text style={styles.fontsiginandsignup}>Sign In</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-          <View style={styles.wrapDot}>
-            {slides?.map((item, index) => {
-              return (
-                <View>
-                  {imgActive !== 3 && (
-                    <View>
-                      <Text
-                        // key={index}
-                        style={
-                          imgActive == index ? styles.dotActive : styles.dot
-                        }>
-                        ◉
-                      </Text>
-                    </View>
-                  )}
-                </View>
-              );
-            })}
-            {imgActive !== 3 && (
-              <View style={[styles.footer, styles.top]}>
-                <TouchableOpacity onPress={()=>navigation.navigate("signupscreen")}>
-                  <Text style={styles.fontsiginandsignup}>Sign Up</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  srollwidth: {
-    width: deviceWidth,
-  },
-  footer: {
-    //  marginTop: deviceHeight * 0.09,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingLeft: deviceWidth * 0.05,
-    paddingRight: deviceWidth * 0.05,
-    // borderTopWidth: deviceWidth * 0.001,
-    //  paddingTop: deviceHeight * 0.02,
-  },
-  paginatiioncon: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  dot: {
-    margin: 3,
-    color: colors.secondary,
-    opacity: 0.6,
-    fontSize: fontSize.h2,
-  },
-  wrapDot: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    // position:'absolute',
-    // top:sizes.screenHeight * 0.19,
-  },
-  wrap: {
-    width: sizes.screenWidth * 0.96,
-  },
-  dotActive: {
-    margin: 3,
-    color: colors.secondary,
-    fontSize: fontSize.h2,
-  },
-  top: {
-    top: sizes.screenHeight * 0.015,
-  },
-  fontsiginandsignup: {
-    fontSize: fontSize.h5,
-    color: colors.secondary,
-  },
-});
-
-export default Intro;
+       })}
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+   srollwidth: {
+     width: deviceWidth,
+   },
+   footer: {
+    //  marginTop: deviceHeight * 0.02,
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     paddingLeft: deviceWidth * 0.05,
+     paddingRight: deviceWidth * 0.05,
+     borderTopWidth: deviceWidth * 0.001,
+     paddingTop: deviceHeight * 0.02,
+   },
+   paginatiioncon: {
+     flexDirection: 'row',
+     justifyContent: 'center',
+     alignItems: 'center',
+   },
+   dot: {
+     width: deviceWidth * 0.04,
+     height: deviceHeight * 0.02,
+     backgroundColor: 'red',
+     borderRadius: deviceWidth * 0.5,
+     marginLeft: deviceWidth * 0.02,
+   },
+   fontsiginandsignup: {
+     fontSize: 25,
+     color: 'red',
+   },
+ });
+ 
+ export default App;
